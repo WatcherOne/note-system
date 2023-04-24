618c3fa6dafea6bf353bdb9293b7333d74a14cbc
zhubo <286154864@qq.com>
2023-04-24 11:51:26 +0800

    fixed upload dist config

--- web/upload.js
+++ web/upload.js
@@ -1,12 +1,13 @@
-import client from 'scp2'
+const client = require('scp2')
 
 // 第一个参数：本地打包文件位置
 client.scp('./dist/', {
-    host: '',          // 服务器的IP地址
-    port: 22,          // 服务器端口, 默认一般22
-    username: 'root',  // 服务器用户，默认一般root
-    password: '',      // 服务器登录密码
-    path: '/home/',    // 项目部署服务器目标位置
+    host: '39.106.78.182',          // 服务器的IP地址
+    port: 22,                       // 服务器端口, 默认一般22
+    username: 'root',               // 服务器用户，默认一般root
+    password: 'xxxx',               // 服务器登录密码
+    // 项目部署服务器目标位置
+    path: '/home/ecs-assist-user/project/note-system/dist/',
 }, err => {
     if (err) {
         console.log('部署前端文件失败：\n', err)
